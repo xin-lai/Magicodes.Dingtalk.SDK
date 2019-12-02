17d53ffb33b4672df0c2623593f64a21fe629b12
方华 <hua.fang@xin-lai.com>
2019-12-02 16:36:07 +0800

    添加部分钉盘文件管理API

---
+++ src/Magicodes.Dingtalk.SDK/File/Dto/SingleUploadResult.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Dingtalk.SDK.File.Dto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SingleUploadResult : ApiResultBase
+    {
+        /// <summary>
+        /// 文件存储id
+        /// </summary>
+        [JsonProperty("media_id")]
+        public string MediaId { get; set; }
+    }
+}
---
+++ src/Magicodes.Dingtalk.SDK/File/Dto/SubTransactionUploadResult.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Dingtalk.SDK.File.Dto
+{
+    /// <summary>
+    /// 提交文件上传事务返回结果
+    /// </summary>
+    public class SubTransactionUploadResult : ApiResultBase
+    {
+        /// <summary>
+        /// 文件存储id
+        /// </summary>
+        [JsonProperty("media_id")]
+        public string MediaId { get; set; }
+    }
+}
