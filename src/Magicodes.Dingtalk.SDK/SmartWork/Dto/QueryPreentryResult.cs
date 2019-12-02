082508aa124d87a2f71b5646f4d041147106a251
方华 <hua.fang@xin-lai.com>
2019-12-02 14:59:07 +0800

    添加部分员工API

---
+++ src/Magicodes.Dingtalk.SDK/SmartWork/Dto/QueryPreentryResult.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magicodes.Dingtalk.SDK.SmartWork.Dto
+{
+    /// <summary>
+    /// 查询企业待入职员工列表返回结果
+    /// </summary>
+    public class QueryPreentryResult : ApiResultBase
+    {
+        /// <summary>
+        /// 调用是否成功
+        /// </summary>
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        /// <summary>
+        /// 员工花名册信息结果列表
+        /// </summary>
+        [JsonProperty("result")]
+        public Result ResultInfo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class Result
+        {
+            /// <summary>
+            /// 下一次分页调用的offset值，当返回结果里没有nextCursor时，表示分页结束
+            /// </summary>
+            [JsonProperty("next_cursor")]
+            public int? NextCursor { get; set; }
+
+            /// <summary>
+            /// 员工userid列表
+            /// </summary>
+            [JsonProperty("data_list")]
+            public List<string> DataList { get; set; }
+        }
+    }
+}
